e35287197c015902a817927dcd9854e7a6701e98
haotongluo <3070282578@qq.com>
2023-02-10 10:17:38 +0800

    第一次提交

---
+++ build/hello_vscode/catkin_generated/installspace/hello_vscode_p.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import rospy
+
+if __name__ == "__main__":
+    rospy.init_node("hello-p")
+    rospy.loginfo("hello python")
---
+++ src/hello_vscode/scripts/test.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+
+import rospy
+
+if __name__ == "__main__":
+    rospy.init_node("hello-p")
+    rospy.loginfo("hello python11111111111")
